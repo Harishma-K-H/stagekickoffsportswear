476dc486f3ae1a2201048c0f0d0fa0d740619ae2
Harishma <harishma@signroots.com>
2025-05-28 15:20:48 +0530

    import issue

--- src/utils/staff/paidAmount.ts
+++ src/utils/staff/paidAmount.ts
@@ -1,6 +1,11 @@
 // Calculate total paid amount and current balance
-export const paidAmount = (payment_details: any) => {
+export const paidAmount = (payment_details: any[]) => {
   return payment_details?.reduce((sum: number, payment: any) => {
-    return sum + parseFloat(payment?.paid_amount);
+    // Skip refund entries
+    if (payment?.payment_method === 'REFUND') {
+      return sum;
+    }
+
+    return sum + parseFloat(payment?.paid_amount || '0');
   }, 0);
 };
