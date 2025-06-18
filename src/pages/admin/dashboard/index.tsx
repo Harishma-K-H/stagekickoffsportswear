ecd358b4d7736408d388e27136bfde38833801a8
Harishma <harishma@signroots.com>
2025-06-18 15:00:03 +0530

    dashboard package issue

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
