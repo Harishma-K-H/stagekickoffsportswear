9411ba03e405d9d6f4bf3f9d5e2a07c9a16b5c23
Harishma <harishma@signroots.com>
2025-08-11 14:45:16 +0530

    fix:check env issue

--- src/Services/ApiService/Api.config.ts
+++ src/Services/ApiService/Api.config.ts
@@ -1,3 +1,8 @@
+// export const API_CONFIG = {
+//   baseURL: import.meta.env.VITE_API_BASE_PATH,
+// };
+import config from '../config.service';
+
 export const API_CONFIG = {
-  baseURL: import.meta.env.VITE_API_BASE_PATH,
+  baseURL: config.get('API_BASE_PATH'),
 };
--- src/Services/config.service.ts
+++ src/Services/config.service.ts
@@ -1,9 +1,37 @@
+// import { Config } from '@models/Config';
+// import { get as _get } from 'lodash';
+
+// const config: Config = {
+//   env: {
+//     API_BASE_PATH: import.meta.env.VITE_API_BASE_PATH,
+//   },
+//   get(key: string, type: string = 'env') {
+//     if (type === 'env') {
+//       return _get(config, `env.${key}`, null);
+//     }
+//   },
+// };
+
+// export default config;
+
 import { Config } from '@models/Config';
 import { get as _get } from 'lodash';
 
+const hostname = window.location.hostname;
+
+const API_BASE_PATH = (() => {
+  if (hostname === '139.59.71.247') {
+    return 'http://139.59.71.247/api';
+  } else if (hostname === 'stage.kickoffsportswear.app') {
+    return 'https://stage.kickoffsportswear.app/api';
+  } else {
+    return import.meta.env.VITE_API_BASE_PATH;
+  }
+})();
+
 const config: Config = {
   env: {
-    API_BASE_PATH: import.meta.env.VITE_API_BASE_PATH,
+    API_BASE_PATH,
   },
   get(key: string, type: string = 'env') {
     if (type === 'env') {
