adeb411811d9b1486b0b81a07bb3e7e15890295c
Harishma K H <harishmahridas777@gmail.com>
2026-02-13 10:04:18 +0530

    fix:router issue

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import { ConfigProvider } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router';
+// import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App.tsx';
